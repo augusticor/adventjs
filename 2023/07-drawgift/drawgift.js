f960324cd2266a47739e290121b68596d76075f2
augusticor <rojascruzoscar@gmail.com>
2024-05-22 18:50:49 -0500

    Challenge 07 draw 3d gift

--- 2023/07-drawgift/drawgift.js
+++ 2023/07-drawgift/drawgift.js
@@ -1,11 +1,34 @@
+/**
+ * Función que dibuja un regalo 3d con el tamaño y símbolo especificado
+ * @param {number} size Tamaño del regalo 3d, primera fila
+ * @param {string} symbol Símbolo que se usará para dibujar el regalo
+ * @returns {string} Regresa un string con el regalo dibujado
+ */
 function drawGift(size, symbol) {
-  // Code here
-  return "#\n";
+  if (size === 1) return "#\n";
+
+  const padStartSize = size + (size - 1);
+  const top = "#".repeat(size).padStart(padStartSize);
+  const repeatAmount = size - 2;
+
+  // Armar la parte del medio
+  const middleConstant = "#" + symbol.repeat(repeatAmount) + "#";
+  let topMiddle = top + "\n";
+  for (let i = 0; i < repeatAmount; i++) {
+    topMiddle +=
+      (middleConstant + symbol.repeat(i) + "#").padStart(padStartSize) + "\n";
+  }
+
+  const center = "#".repeat(size) + symbol.repeat(repeatAmount) + "#";
+
+  // Invertir la cadena middle
+  const bottomMiddle = topMiddle
+    .split("\n")
+    .reverse()
+    .map((line) => line.trim())
+    .join("\n");
+
+  return topMiddle + center + bottomMiddle + "\n";
 }
 
 export default drawGift;
-
-// 5 / 2 = 2.5 rounded up = 3
-// 4 / 2 = 2 rounded up = 2
-// 1 / 2 = 0.5 rounded up = 1
-// 2 / 2 = 1 rounded up = 1
---
+++ 2023/07-drawgift/drawgift.test.js
@@ -0,0 +1,63 @@
+import { describe, expect, it } from "vitest";
+import drawGift from "./drawgift";
+
+describe("Challenge 7 draw 3d gift", () => {
+  it("Should return expected cube with level is 4 and symbol +'", () => {
+    const expectedCube =
+      "   ####\n" +
+      "  #++##\n" +
+      " #++#+#\n" +
+      "####++#\n" +
+      "#++#+#\n" +
+      "#++##\n" +
+      "####\n";
+
+    expect(drawGift(4, "+")).toBe(expectedCube);
+  });
+
+  it("Should return expected cube with level is 5 and symbol *'", () => {
+    const expectedCube =
+      "    #####\n" +
+      "   #***##\n" +
+      "  #***#*#\n" +
+      " #***#**#\n" +
+      "#####***#\n" +
+      "#***#**#\n" +
+      "#***#*#\n" +
+      "#***##\n" +
+      "#####\n";
+
+    expect(drawGift(5, "*")).toBe(expectedCube);
+  });
+
+  it("Should return expected cube with level is 1 and symbol ^'", () => {
+    const expectedCube = "#\n";
+
+    expect(drawGift(1, "^")).toBe(expectedCube);
+  });
+
+  it("Should return expected cube with level is 2 and symbol &'", () => {
+    const expectedCube = " ##\n" + "###\n" + "##\n";
+
+    expect(drawGift(2, "&")).toBe(expectedCube);
+  });
+
+  it("Should return expected cube with level is 3 and symbol *'", () => {
+    const expectedCube = "  ###\n" + " #*##\n" + "###*#\n" + "#*##\n" + "###\n";
+
+    expect(drawGift(3, "*")).toBe(expectedCube);
+  });
+
+  it("Should return expected cube with level is 4 and symbol ''", () => {
+    const expectedCube =
+      "   ####\n" +
+      "  #  ##\n" +
+      " #  # #\n" +
+      "####  #\n" +
+      "#  # #\n" +
+      "#  ##\n" +
+      "####\n";
+
+    expect(drawGift(4, " ")).toBe(expectedCube);
+  });
+});
