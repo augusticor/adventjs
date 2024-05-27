613367cfc653a8874695820a55e379e852eeb541
augusticor <rojascruzoscar@gmail.com>
2024-05-27 15:44:33 -0500

    Fix edge case for challenge 09 adjust lights

--- 2023/09-adjustlights/adjustlights.js
+++ 2023/09-adjustlights/adjustlights.js
@@ -2,19 +2,27 @@
  * Función que recibe una secuencia de luces y devuelve la cantidad de luces que hay que cambiar para que sigan la secuencia
  * @param {Array<"🔴" | "🟢">} lights Secuencia de luces en un array
  * @returns {number} Cantidad de luces que hay que cambiar porque no siguen la secuencia
- * @example adjustLights(|) -> Only position 4 changes to '🔴'
+ * @example adjustLights(['🟢', '🔴', '🟢', '🟢', '🟢']) -> Solo la cuarta posicion cambia a '🔴'
  */
 function adjustLights(lights) {
-  let counter = 0;
-  
-  for (let i = lights.length - 1; i > 0; i --) {
-    if (lights[i] === lights[i - 1]) {
-      counter++;
-      lights[i - 1] = lights[i - 1] === "🟢" ? "🔴" : "🟢";
-    }
+  const redLight = "🔴";
+  const greenLight = "🟢";
+  let counterFirst = 0;
+  let counterSecond = 0;
+
+  for (let i = 0; i < lights.length; i += 2) {
+    if (lights[i] !== undefined && lights[i] !== greenLight) counterFirst++;
+
+    if (lights[i + 1] !== undefined && lights[i + 1] !== redLight)
+      counterFirst++;
+
+    if (lights[i] !== undefined && lights[i] !== redLight) counterSecond++;
+
+    if (lights[i + 1] !== undefined && lights[i + 1] !== greenLight)
+      counterSecond++;
   }
 
-  return counter;
+  return Math.min(counterFirst, counterSecond);
 }
 
 export default adjustLights;
